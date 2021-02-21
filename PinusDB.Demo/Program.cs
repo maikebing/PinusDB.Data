918bca74bdd4e34a9692b402f3be431ddc969f0d
MysticBoy <mysticboy@live.com>
2021-02-21 14:56:20 +0800

    修正

--- PinusDB.Demo/Program.cs
+++ PinusDB.Demo/Program.cs
@@ -20,8 +20,13 @@ namespace PinusDB.Demo
                 using (var connection = new PinusConnection(builder.ConnectionString))
                 {
                     connection.Open();
+                    if (connection.CreateCommand("select count(*) from sys_table where tabname ='test'").ExecuteScalar() as long? == 1)
+                    {
+                        Console.WriteLine("删除表", connection.CreateCommand($"DROP TABLE test").ExecuteNonQuery());
+                        Console.WriteLine("删除设备", connection.CreateCommand($"DELETE FROM sys_dev WHERE tabname='test'").ExecuteNonQuery());
+                    }
                     Console.WriteLine("创建遥测数据表", connection.CreateCommand(
-                        @" CREATE TABLE test (  devid bigint,                  tstamp datetime, val01 bool, val02 bigint, val03 double, val04 real2 )").ExecuteNonQuery());
+                        @" CREATE TABLE test (  devid bigint,tstamp datetime, val01 bool, val02 bigint, val03 double, val04 real2 )").ExecuteNonQuery());
                     Console.WriteLine("创建设备", connection.CreateCommand(@"INSERT INTO sys_dev(tabname, devid) VALUES('test',1)").ExecuteNonQuery());
                     Console.WriteLine("添加数据", connection.CreateCommand(@"INSERT INTO test(devid,tstamp,val01,val02,val03) VALUES(1, now(), true, 1, 1.1111)").ExecuteNonQuery());
                     var cmd_select = connection.CreateCommand();
